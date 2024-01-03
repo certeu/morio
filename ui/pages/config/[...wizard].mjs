d16fd01a662ff45ee7e1b18f4d3f65003159caf7
Joost De Cock <joost.decock@cert.europa.eu>
2024-01-03 17:24:58 +0100

    [ui] Load ConfigurationWizard dynamically
    This avoids hydration errors as we keep the view state in the URL
    location which tends to cause issues with SSR.

--- ui/pages/config/[...wizard].mjs
+++ ui/pages/config/[...wizard].mjs
@@ -1,13 +1,22 @@
+import dynamic from 'next/dynamic'
 import { PageWrapper } from 'components/layout/page-wrapper.mjs'
 import { SplashLayout } from 'components/layout/splash.mjs'
 import { ConfigurationWizard, viewAsConfigPath } from 'components/config/wizard.mjs'
 
 const prefix = '/config'
 
+/*
+ * Avoid hydration woes with a dynamic import and disabled SSR rendering
+ */
+const DynamicConfigurationWizard = dynamic(
+  () => import('components/config/wizard.mjs').then((mod) => mod.ConfigurationWizard),
+  { ssr: false }
+)
+
 const ConfigPage = (props) => {
   return (
     <PageWrapper {...props}>
-      <ConfigurationWizard preloadView={props.view} splash={false} prefix={prefix} />
+      <DynamicConfigurationWizard preloadView={props.view} splash={false} prefix={prefix} />
     </PageWrapper>
   )
 }
@@ -17,7 +26,7 @@ export default ConfigPage
 export const getStaticProps = ({ params }) => ({
   props: {
     title: 'Morio Configuration',
-    view: params.wizard.join('/'),
+    view: params.wizard.join('.'),
     page: ['config', ...params.wizard],
   },
 })
--- ui/pages/setup/[...wizard].mjs
+++ ui/pages/setup/[...wizard].mjs
@@ -1,13 +1,21 @@
+import dynamic from 'next/dynamic'
 import { PageWrapper } from 'components/layout/page-wrapper.mjs'
 import { SplashLayout } from 'components/layout/splash.mjs'
-import { ConfigurationWizard } from 'components/config/wizard.mjs'
+
+/*
+ * Avoid hydration woes with a dynamic import and disabled SSR rendering
+ */
+const DynamicConfigurationWizard = dynamic(
+  () => import('components/config/wizard.mjs').then((mod) => mod.ConfigurationWizard),
+  { ssr: false }
+)
 
 const ConfigWizardPage = (props) => {
   return (
     <PageWrapper {...props} layout={SplashLayout}>
       <div className="py-12 px-4">
-        <h1 className="text-center">Configure Morio</h1>
-        <ConfigurationWizard
+        <h1 className="text-center">{props.title}</h1>
+        <DynamicConfigurationWizard
           initialSetup
           preloadView="morio/node_count"
           splash
