2dbf1580b8a46aebaf1d1fccfe7e0d50bc903aae
Joost De Cock <joost@joost.at>
2024-03-25 17:47:02 +0100

    fix: Fix in version extraction with sed

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -40,7 +40,7 @@ else
   REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 
   # Grab the Morio version from package.json
-  VERSION=`sed 's/version/VERSION/' $REPO/package.json | grep VERSION | tr -d 'VERSION [:blank:] ["] [:] [,]'`
+  VERSION=`sed 's/\"version\"/\"VERSION\"/' $REPO/package.json | grep VERSION | tr -d 'VERSION [:blank:] ["] [:] [,]'`
 
   # Now build the container
   cd $REPO/$1
