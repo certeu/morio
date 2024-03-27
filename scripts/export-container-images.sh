fe98cbcb69276b031a1ef73e35175b72280b3a45
Joost De Cock <joost@joost.at>
2024-03-27 17:24:05 +0100

    chore: Run production code via PM2 inside the container

---
+++ scripts/export-container-images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Check that a specific version
+if [ -z "$1" ];
+then
+  # Figure out the repository root
+  REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
+
+  # Grab the Morio version from package.json
+  VERSION=`sed 's/\"version\"/\"VERSION\"/' $REPO/package.json | grep VERSION | tr -d 'VERSION [:blank:] ["] [:] [,]'`
+else
+  VERSION=$1
+fi
+
+SERVICES=( api core ui )
+
+for SERVICE in "${SERVICES[@]}"
+do
+  FILE=$REPO/data/data/tmp_static/morio-$SERVICE.tar
+  echo "🚚 Exporting image morio/$SERVICE:$VERSION"
+  sudo docker image save morio/$SERVICE:$VERSION -o $FILE
+  echo "✅ Exported image morio/$SERVICE:$VERSION to $FILE"
+done
