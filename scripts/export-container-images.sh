a9e433c0773f13976cd9e9a707e839e9852c7c51
Joost De Cock <joost@joost.at>
2024-04-03 16:35:13 +0200

    chore: Include version in exported images

--- scripts/export-container-images.sh
+++ scripts/export-container-images.sh
@@ -12,13 +12,13 @@ else
   VERSION=$1
 fi
 
-SERVICES=( api core ui )
+SERVICES=( api core ui dbuilder )
 
 mkdir -p $REPO/data/data/tmp_static
 
 for SERVICE in "${SERVICES[@]}"
 do
-  FILE=$REPO/data/data/tmp_static/morio-$SERVICE.tar
+  FILE=$REPO/data/data/tmp_static/morio-$SERVICE-$VERSION.tar
   echo "🚚 Exporting image morio/$SERVICE:$VERSION"
   sudo docker image save morio/$SERVICE:$VERSION -o $FILE
   echo "✅ Exported image morio/$SERVICE:$VERSION to $FILE"
