10a10cd4a8570877305facd2627e3a90a8b62f45
Joost De Cock <joost@joost.at>
2024-04-24 19:11:08 +0200

    chore: Do not ask confirmation in destroy script

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -2,30 +2,9 @@
 
 DIR=`pwd`
 
-echo
-echo
-echo "This will destroy you Morio deployment/configuration."
-echo
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "!!! Careful, there is no way back from this !!!"
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo
-echo "Enter y to continue. Anything else and we'll bail out."
-read -p " =>  " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo
-  echo "Hope you know what you're doing"
-  npm run dev:clean
-  sudo rm -rf ${DIR}/data/*
-  # Also remove auto-generated files
-  rm -f ${DIR}/clients/linux/etc/morio/audit/config-template.yml
-  rm -f ${DIR}/clients/linux/etc/morio/logs/config-template.yml
-  rm -f ${DIR}/clients/linux/etc/morio/metrics/config-template.yml
-else
-  echo
-  echo
-  echo "Phew, bailing out."
-  echo
-fi
-
+npm run dev:clean
+sudo rm -rf ${DIR}/data/*
+# Also remove auto-generated files
+rm -f ${DIR}/clients/linux/etc/morio/audit/config-template.yml
+rm -f ${DIR}/clients/linux/etc/morio/logs/config-template.yml
+rm -f ${DIR}/clients/linux/etc/morio/metrics/config-template.yml
