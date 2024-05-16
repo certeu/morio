332a63e4cf1b8503c77f6a0acc3fd396ca26bfe8
Joost De Cock <joost.decock@cert.europa.eu>
2024-05-16 10:43:35 +0200

    wip: Work on clustering

--- api/src/controllers/core.mjs
+++ api/src/controllers/core.mjs
@@ -281,7 +281,7 @@ Controller.prototype.getPresets = async (req, res) => {
  * @param {object} res - The response object from Express
  */
 Controller.prototype.decrypt = async (req, res) => {
-  const [status, result] = await store.core.post(`/decrypt`, bodyPlusHeaders)
+  const [status, result] = await store.core.post(`/decrypt`, bodyPlusHeaders(req))
 
   return res.status(status).send(result)
 }
@@ -293,7 +293,7 @@ Controller.prototype.decrypt = async (req, res) => {
  * @param {object} res - The response object from Express
  */
 Controller.prototype.encrypt = async (req, res) => {
-  const [status, result] = await store.core.post(`/encrypt`, bodyPlusHeaders)
+  const [status, result] = await store.core.post(`/encrypt`, bodyPlusHeaders(req))
 
   return res.status(status).send(result)
 }
@@ -306,7 +306,7 @@ Controller.prototype.encrypt = async (req, res) => {
  * @param {tring} type - The type of client package (one of deb, rpm, msi, or pkg)
  */
 Controller.prototype.buildClientPackage = async (req, res, type) => {
-  const [status, result] = await store.core.post(`/pkgs/clients/${type}/build`, bodyPlusHeaders)
+  const [status, result] = await store.core.post(`/pkgs/clients/${type}/build`, bodyPlusHeaders(req))
 
   return res.status(status).send(result)
 }
@@ -332,7 +332,7 @@ Controller.prototype.getJwks = async (req, res) => {
 Controller.prototype.joinCluster = async (req, res, path) => {
   store.log.info('Received request to join cluster')
   console.log({ apiBody: req.body })
-  const [status, result] = await store.core.post(`/cluster/join`, bodyPlusHeaders)
+  const [status, result] = await store.core.post(`/cluster/join`, bodyPlusHeaders(req))
 
   return res.status(status).send(result)
 }
