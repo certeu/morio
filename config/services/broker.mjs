25058b4afa3f84298b67e2ad566fb4b686ad5732
Joost De Cock <joost.decock@cert.europa.eu>
2024-07-20 16:28:50 +0200

    wip: Working on RedPanda bootstrap

--- config/services/broker.mjs
+++ config/services/broker.mjs
@@ -45,7 +45,6 @@ export const resolveServiceConfiguration = ({ utils }) => {
         '19082:19082',
         '19644:19644',
         '9092:19092',
-        '19092:19092',
         '33145:33145',
       ],
       // Environment
