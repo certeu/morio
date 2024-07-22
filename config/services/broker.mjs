48bea5f40ff815a8e72e8637d296d672082fdf86
Joost De Cock <joost.decock@cert.europa.eu>
2024-07-22 12:43:50 +0200

    wip: Work on broker config

--- config/services/broker.mjs
+++ config/services/broker.mjs
@@ -70,7 +70,7 @@ export const resolveServiceConfiguration = ({ utils }) => {
       command: [
         'redpanda',
         'start',
-        `--kafka-addr external://${utils.getNodeFqdn()}:${PORTS.EXT}`,
+        `--kafka-addr external://0.0.0.0:${PORTS.EXT}`,
         `--advertise-kafka-addr external://${utils.getNodeFqdn()}:${PORTS.EXT}`,
         `--rpc-addr 0.0.0.0:33145`,
         //`--kafka-addr internal://0.0.0.0:${PORTS.INT},external://${utils.getNodeFqdn()}:${PORTS.EXT}`,
