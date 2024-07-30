20d433f4842e2d58b37480c567922c1ebd46c3a9
Joost De Cock <joost.decock@cert.europa.eu>
2024-07-30 12:58:10 +0200

    wip: Troubleshooting cluster heartbeat

--- core/src/lib/cluster.mjs
+++ core/src/lib/cluster.mjs
@@ -239,7 +239,6 @@ const sendHeartbeat = async (fqdn, broadcast = false, justOnce = false) => {
   const start = Date.now()
   let data
   log.todo(`About to send heartbeat to ${fqdn}, cluster is: ${utils.getClusterUuid()}`)
-  if (!utils.getClusterUuid()) utils.dumpStore()
   try {
     if (broadcast) log.trace(`Broadcast heartbeat to ${fqdn}`)
     data = await testUrl(`https://${fqdn}/-/core/cluster/heartbeat`, {
