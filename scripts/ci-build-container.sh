e00ad277982bc17541a57c9de566fe486ae0eb11
Joost De Cock <joost.decock@cert.europa.eu>
2024-08-08 17:08:09 +0200

    wip: Work on building container images

--- scripts/ci-build-container.sh
+++ scripts/ci-build-container.sh
@@ -76,7 +76,13 @@ else
   # Create a folder for the build context
   rm -rf $REPO/build-context
   mkdir -p $REPO/build-context
-  cd $REPO/$IMAGE && tar -ch -f $REPO/build-context.tar . && cd $REPO/build-context && tar -xf $REPO/build-context.tar . && cd $REPO
+  if [[ $IMAGE == *"builder" ]]
+  then
+    cd $REPO/builders/$IMAGE
+  else
+    cd $REPO/$IMAGE
+  fi
+  tar -ch -f $REPO/build-context.tar . && cd $REPO/build-context && tar -xf $REPO/build-context.tar . && cd $REPO
 
   # Now build the OCI image
   buildah build-using-dockerfile \
