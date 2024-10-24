1ca882ee2cc71eef2f95191a7768a020e3af683e
Joost De Cock <joost@joost.at>
2024-10-24 09:46:28 +0200

    chore: OCI pull script updated to new versions

--- scripts/pull-oci-images.sh
+++ scripts/pull-oci-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-docker pull docker.redpanda.com/redpandadata/redpanda:v24.2.5
-docker pull smallstep/step-ca:0.27.4
-docker pull docker.elastic.co/logstash/logstash:8.15.1
+docker pull docker.redpanda.com/redpandadata/redpanda:v24.2.7
+docker pull smallstep/step-ca:0.27.5
+docker pull docker.elastic.co/logstash/logstash:8.15.3
 docker pull docker.redpanda.com/redpandadata/console:v2.7.2
-docker pull rqlite/rqlite:8.30.5
-docker pull traefik:v3.1.4
-docker pull docker.elastic.co/beats/heartbeat:8.15.1
+docker pull rqlite/rqlite:8.32.3
+docker pull traefik:v3.1.6
+docker pull docker.elastic.co/beats/heartbeat:8.15.3
