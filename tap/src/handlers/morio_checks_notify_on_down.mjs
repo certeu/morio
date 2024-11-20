46efdfdb8beb4478a82f35461d1a619c1599d254
Joost De Cock <joost@joost.at>
2024-11-20 14:35:16 +0100

    wip: More stream processing

--- tap/src/handlers/morio_checks_notify_on_down.mjs
+++ tap/src/handlers/morio_checks_notify_on_down.mjs
@@ -1,14 +1,98 @@
 /*
  * This is a Morio handler to create a notification when something is down
  */
+
+/*
+ * Various healthcheck values that all mean things are ok, or 'up'
+ */
+const upValues = [ "1", "up", "green" ]
+
+/*
+ * This is a Morio handler to create a notification when something is down
+ */
 const handler = {
   topic: 'checks',
   filter: ({ data={} }) => data.summary?.status === "down",
   method: ({ data }, tools) => {
-    // Do something clever here
-    tools.log.warn(data.summary, 'Something is down!')
+    /*
+     * Only handle data that is in the correct format
+     */
+    if (!data?.monitor?.id || !data?.monitor?.name) return tools.log.debug(data, 'Invalid healthcheck data')
+
+    /*
+     * Figure out what's going on.
+     *   up: Whether the healthcheck is up (ok) or not (0 or 1)
+     *   took: Time in milliseconds that it took (useful observability signal)
+     *   days: Days before certificate expiry (only for https checks)
+     */
+    const [up, took, days] = healthcheckSummary(data, tools)
+    if (!up) tools.notify({
+      context: tools.context('healthcheck', data.monitor.type, data.monitor.id, tools.escape(data.url?.full)),
+      host: data.host?.id,
+      module: data.morio?.module?.name,
+      tags: [ 'healthcheck', 'down', data.monitor.type, data.monitor.id ],
+      time: data['@timestamp'],
+      title: `Healthcheck failed: ${data.url?.full}`,
+      type: `${data.monitor.type}.healthcheck.down`,
+    })
+
+    /*
+     * Can't do a simple if (!days) here because days can be zero
+     */
+    if (days !== null) {
+      // FIXME: Make this treshold configurable
+      if (days < 5) tools.alarm({
+        context: tools.context(`tls.certificate.${tools.escape(data.url.full)}`),
+        host: data?.url?.domain,
+        module: data?.morio?.module?.name,
+        title: `⏳ Certificate will expire in ${days} days: ${data?.url?.full}`,
+        type: 'tls.certificate.expiry.imminent',
+        tags: ['tls','certificate','expiry'],
+      })
+      // FIXME: Make this treshold configurable
+      else if (days < 15) tools.notify({
+        context: tools.context(`tls.certificate.${tools.escape(data.url.full)}`),
+        host: data?.url?.domain,
+        module: data?.morio?.module?.name,
+        tags: ['tls','certificate','expiry'],
+        title: `⏳ Certificate will expire in ${days} days: ${data?.url?.full}`,
+        type: 'tls.certificate.expiry.approaching',
+      })
+    }
   }
 }
 
 export default handler
 
+/*
+ * Takes healthcheck data and returns and array with the key observations
+ *
+ * @param {object} data - The healthcheck data
+ * @return {array} summary - An array holding:
+ *   - up (1 or 0)
+ *   - the ms it took
+ *   - days until the certificate expires
+ */
+function healthcheckSummary (data, tools) {
+  return [
+    (upValues.indexOf(data.monitor.status.toLowerCase()) !== -1) ? 1 : 0,
+    Math.ceil(data.monitor.duration.us/1000),
+    (data.monitor.type === 'http' && data.tls && data.url?.schema === 'https')
+      ? checkCertificateExpiry(data, tools)
+      : null
+  ]
+}
+
+/*
+ * Checks certificate validity
+ *
+ * @param {object} data - The healthcheck data
+ */
+const checkCertificateExpiry = (data, tools) => {
+  const seconds = Math.floor(
+    (new Date(data.tls.certificate_not_valid_after).getTime()/1000)
+    - tools.now()
+  )
+
+  return Math.floor(seconds / (24 * 3600))
+}
--- tap/src/tools.mjs
+++ tap/src/tools.mjs
@@ -1,37 +1,164 @@
+import crypto from 'crypto'
+import querystring from 'querystring'
 import pino from 'pino'
 
 /*
- * Export log object for logging via the Pino logger
+ * Export log object on its own
  */
 export const log = pino({ name: 'tap', level: 20 })
-
 
 /*
  * Tools are exported as a single object
  * so we can pass it to processor functions
+ * Functions are defined lower down
  */
 export const tools = {
-  /*
-   * Log object for logging via the logger
-   */
+  alarm,
+  context,
+  escape: querystring.escape,
+  event,
+  hash,
+  key,
   log,
+  ms2s,
+  notify,
+  now,
+  timestamp,
+}
 
-  /*
-   * Returns current timestamp in seconds
-   *
-   * @return {number} s - Current timestamp in seconds
-   */
-  now: function () {
-    return Math.floor(Date.now() / 1000)
-  },
+/*
+ *
+ * TOOLS METHODS
+ *
+ */
 
+/*
+ * Creates an alarm
+ */
+function alarm (data) {
+  return produceStructuredMessage('alarm', data)
+}
+
+/*
+ * Generates a context key
+ * This is the same as a cache key, but uses '.' as spacer
+ */
+function context (...data) {
+  return generateKey(data, '.')
+}
+
+/*
+ * Creates an event
+ */
+function event (data) {
+  return produceStructuredMessage('event', data)
+}
+
+/*
+ * Hash method to ensure people use consistent hashes
+ *
+ * @param {string} input - The input to hash. A scalar is expected but we will cast to string if you pass a non-scalar.
+ */
+function hash (input) {
   /*
-   * Returns current timestamp in milliseconds
-   *
-   * @return {number} ms - Current timestamp in milliseconds
+   * Ensure this 'just works' even when passing an object or array
    */
-  timestamp: function () {
-    return Date.now()
+  if (typeof input === 'object') input = JSON.stringify(input)
+  return crypto.createHash('sha256').update(input, 'utf-8').digest('hex')
+}
+
+/*
+ * Generates a cache key
+ * This is the same as a context key, but uses '|' as spacer
+ */
+function key (...data) {
+  return generateKey(data, '|')
+}
+
+/*
+ * Converts milliseconds to seconds
+ */
+function ms2s (ms) {
+  return Math.floor(ms / 1000)
+}
+
+/*
+ * Creates a notification
+ */
+function notify (data) {
+  return produceStructuredMessage('notification', data)
+}
+
+/*
+ * Returns current timestamp in seconds
+ *
+ * @return {number} s - Current timestamp in seconds
+ */
+function now () {
+  return Math.floor(Date.now() / 1000)
+}
+
+/*
+ * Returns current timestamp in milliseconds
+ *
+ * @return {number} ms - Current timestamp in milliseconds
+ */
+function timestamp () {
+  return Date.now()
+}
+
+
+/*
+ *
+ * HELPERS
+ *
+ */
+
+/**
+ * This generates a key, which is a string value
+ *
+ * This is used to consistently generate reproducable IDs from data
+ * This message is variadic, so you can pass as many params as you want.
+ */
+function generateKey(data, spacer) {
+  return data.map(p => p ? String(p).replace(/\./, '_') : 'undefined')
+    .join(spacer)
+    .toLowerCase()
+}
+
+/*
+ * Helper message to produce a structured message to Kafka
+ * This means one of: alarm, event, notify
+ */
+function produceStructuredMessage(msgType, msgData) {
+  if (typeof msgData !== 'object') log.warn(`Invalid ${topic} data`)
+
+  const {
+    context=`${msgType}.context.missing`,
+    data=null,
+    host='hostIdUnavailable',
+    tags=[],
+    time=now(),
+    title=`Untitled ${msgType}`,
+    type=`${msgType}.type.missing`,
+  } = msgData
+
+  // Remember that we want ECS compliant data
+  const msg = {
+    host: { id: host },
+    tags,
+    morio: { }
   }
+  msg.morio[msgType] = { context, data, time, title, type, hash: tools.hash(type+context) }
+
+  return tools.producer.send({
+    topic: msgType + 's',
+    messages: [{
+      value: JSON.stringify(msg),
+      headers: {
+        morio_context: msg.morio[msgType].context
+      }
+    }]
+  })
 }
 
