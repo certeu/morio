571a128d2a6136576a96669310910c925459bae6
serge-tellene <114212556+serge-tellene@users.noreply.github.com>
2025-01-09 19:28:08 +0100

    Update oss.mjs
    Review

--- ui/components/inventory/oss.mjs
+++ ui/components/inventory/oss.mjs
@@ -13,7 +13,7 @@ import { PageLink } from 'components/link.mjs'
 import { ReloadDataButton } from 'components/button.mjs'
 
 /**
- * This compnent renders a table with all IP addresses and allows removal
+ * This component renders a table with all IP addresses and allows removal
  */
 export const OssTable = () => {
   // State
