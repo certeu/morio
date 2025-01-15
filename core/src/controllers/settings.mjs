92123a74b9cbff6830791c976f31925ae17061f4
Joost De Cock <joost@joost.at>
2025-01-15 14:57:17 +0100

    [core] fix: Encrypt secrets passed at initial setup
    This was a bug that prevented secrets getting encrypted on the intial
    setup, they would only get encrypted on subsequent deployed (updates to
    the settings).
    
    Closes #136

--- core/src/controllers/settings.mjs
+++ core/src/controllers/settings.mjs
@@ -14,7 +14,12 @@ import { cloneAsPojo } from '#shared/utils'
 import { log, utils } from '../lib/utils.mjs'
 import { generateCaConfig } from '../lib/services/ca.mjs'
 import { unsealKeyData, loadKeysFromDisk } from '../lib/services/core.mjs'
-import { loadPreseededSettings, ensurePreseededContent, loadClientModules, loadStreamProcessors } from '#shared/loaders'
+import {
+  loadPreseededSettings,
+  ensurePreseededContent,
+  loadClientModules,
+  loadStreamProcessors,
+} from '#shared/loaders'
 import { generateKeySeal, generateRootToken, formatRootTokenResponseData } from '../lib/crypto.mjs'
 
 /**
@@ -236,7 +241,7 @@ const initialSetup = async function (req, settings) {
     if (!preseededSettings) err = { message: 'Failed to construct settings from preseed data' }
     else [valid, err] = await utils.validate(`req.settings.setup`, preseededSettings)
   } else {
-    [valid, err] = await utils.validate(`req.settings.setup`, settings)
+    ;[valid, err] = await utils.validate(`req.settings.setup`, settings)
   }
 
   if (!valid?.cluster)
@@ -358,8 +363,9 @@ const initialSetup = async function (req, settings) {
   /*
    * Now ensure token secrecy before we write to disk
    */
-  if (valid.tokens?.secrets) {
-    valid.tokens.secrets = ensureTokenSecrecy(valid.tokens.secrets)
+  if (saveSettings.tokens?.secrets) {
+    saveSettings.tokens.secrets = ensureTokenSecrecy(saveSettings.tokens.secrets)
+    utils.setSettings(saveSettings)
   }
 
   /*
@@ -446,8 +452,8 @@ const reseedHandler = async function (newSettings = false) {
    * Load the preseeded settings
    */
   let settings = await loadPreseededSettings(
-    (newSettings ? newSettings.preseed : utils.getSettings('preseed')),
-    (newSettings ? newSettings : utils.getSettings()),
+    newSettings ? newSettings.preseed : utils.getSettings('preseed'),
+    newSettings ? newSettings : utils.getSettings(),
     log
   )
 
@@ -470,7 +476,7 @@ const reseedHandler = async function (newSettings = false) {
  * @param {object} settings - The settings to use (could be different from the running settings)
  * @param {object} settings - The (potentially) updated settings
  */
-export async function ensureClientModules (settings) {
+export async function ensureClientModules(settings) {
   return await loadClientModules(settings, log)
 }
 
@@ -480,7 +486,7 @@ export async function ensureClientModules (settings) {
  * @param {object} settings - The current settings
  * @param {object} settings - The (potentially) updated settings
  */
-export async function ensureStreamProcessors (settings) {
+export async function ensureStreamProcessors(settings) {
   /*
    * This will not only load stream processors, but also
    * merge their (default) settings into  the settings object
