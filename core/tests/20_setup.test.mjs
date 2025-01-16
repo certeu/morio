02cdd0da3855c2a798f6f0fc3e6de4721ee41cf6
serge-tellene <114212556+serge-tellene@users.noreply.github.com>
2025-01-16 13:47:32 +0100

    Update 20_setup.test.mjs
    Review

--- core/tests/20_setup.test.mjs
+++ core/tests/20_setup.test.mjs
@@ -21,7 +21,7 @@ describe('Core Setup Tests', () => {
     const result = await core.post('/setup', {
       ...setup,
       // Normally, the proxy adds the headers to the body
-      // Since we're talking to core direcytly, we need to do this ourselves
+      // Since we're talking to core directly, we need to do this ourselves
       headers: { 'x-forwarded-host': getPreset('MORIO_UNIT_TEST_HOST') },
     })
     const d = result[1]
