3a74102e17915081910c1d143c68e4c97c0cdd4e
Joost De Cock <joost@joost.at>
2025-02-06 10:27:40 +0100

    [ui] chore: Drop splunk template example

--- ui/components/settings/templates/connector/lib.mjs
+++ ui/components/settings/templates/connector/lib.mjs
@@ -12,25 +12,6 @@ export const vectorTemplates = {
       type: 'local_morio',
       topics: [],
     },
-    'Splunk (logs)': {
-      type: 'splunk_hec_logs',
-      inputs: ['input-or-transform-id-here'],
-      endpoint: 'hec-endpoint-base-url-here',
-      endpoint_target: 'event',
-      index: '{{ host }}',
-      source: '{{ file }}',
-      timestamp_key: 'timestamp',
-      buffer: {
-        type: 'disk',
-        max_size: 5e9,
-        when_full: 'block',
-      },
-      default_token: '{{ SPLUNK_HEC_TOKEN }}',
-      encoding: {
-        codec: 'json',
-      },
-      sourcetype: '_json',
-    },
   },
   transform: {
     blank: {},
