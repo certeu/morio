c122a5ae228adade5ed28482f5a8f379488aa768
Joost De Cock <joost@joost.at>
2025-02-20 15:46:32 +0100

    fix: Properly label morionet

--- scripts/test-api.sh
+++ scripts/test-api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm run redev
-docker network create morionet || true
+docker network create morionet --labels "morio.network.description=Bridge docker network for morio services" || true
 ./api/tests/start-ldap-server.sh
 ./scripts/sleep 7
 docker exec -it morio-api bash /morio/api/tests/run-unit-tests.sh && npm run redev
