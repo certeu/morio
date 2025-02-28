8db0b75fc572cde1eeb6a79054707e3c09df7bd2
levente232 <leventebajusz614@gmail.com>
2025-02-28 10:56:42 +0000

    remove api directory from api

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -5,7 +5,7 @@ source config/cli.sh
 docker exec -it morio-core bash -c "rm -rf /etc/morio"
 docker exec -it morio-core bash -c "rm -rf /var/log/morio"
 docker exec -it morio-core bash -c "rm -rf /morio/data"
-docker exec -it morio-core bash -c "rm -rf /api"
+docker exec -it morio-api bash -c "rm -rf /api"
 
 docker rm -fv \
   morio-core \
