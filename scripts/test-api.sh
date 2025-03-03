325f8f4a96c67f0de612669ed8ef64d2288d0647
levente232 <leventebajusz614@gmail.com>
2025-03-03 02:39:11 +0000

    commit test.

--- scripts/test-api.sh
+++ scripts/test-api.sh
@@ -3,4 +3,9 @@ npm run redev
 docker network create morionet --label "morio.network.description=Bridge docker network for morio services" || true
 ./api/tests/start-ldap-server.sh
 ./scripts/sleep 7
-docker exec -i morio-api bash /morio/api/tests/run-unit-tests.sh && npm run redev
+if [ -t 1 ]; then
+  docker exec -it morio-api bash /morio/api/tests/run-unit-tests.sh
+else
+  docker exec -i morio-api bash /morio/api/tests/run-unit-tests.sh
+fi
+npm run redev
