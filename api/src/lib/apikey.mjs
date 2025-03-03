9c0f5beec957f5578d3f6eecb106f0d2751ec928
levente232 <leventebajusz614@gmail.com>
2025-03-03 10:28:47 -0500

    Update apikey.mjs

--- api/src/lib/apikey.mjs
+++ api/src/lib/apikey.mjs
@@ -109,6 +109,11 @@ export async function deleteApikey(id = false) {
  * @return {number} status - 200 or 500
  */
 export async function createApikey(data) {
+  if (!data.id) {
+    log.debug('createApikey was called without an id')
+    return false
+  }
+  
   const columns = []
   const params = {}
 
