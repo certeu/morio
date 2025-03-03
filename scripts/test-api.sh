dc722a91e0b4c55a9f865b5768889aaf39302f82
levente232 <leventebajusz614@gmail.com>
2025-03-03 02:26:24 +0000

    test api t option removed

--- scripts/test-api.sh
+++ scripts/test-api.sh
@@ -3,4 +3,4 @@ npm run redev
 docker network create morionet --label "morio.network.description=Bridge docker network for morio services" || true
 ./api/tests/start-ldap-server.sh
 ./scripts/sleep 7
-docker exec -it morio-api bash /morio/api/tests/run-unit-tests.sh && npm run redev
+docker exec -i morio-api bash /morio/api/tests/run-unit-tests.sh && npm run redev
