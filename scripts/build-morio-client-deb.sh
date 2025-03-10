d4caf397ccdeb1af68f9306c363ba0a22d496b31
Joost De Cock <joost@joost.at>
2025-03-10 10:10:04 +0100

    chore: Work on .deb builds

--- scripts/build-morio-client-deb.sh
+++ scripts/build-morio-client-deb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# This script will build the .deb package for moriod
+# This script will build the .deb package for morio (the client)
 # It is created for a CI environment, but should also run fine
 # on a system that supports .deb (a Debian-based distribution)
 #
@@ -16,7 +16,7 @@ if [[ "$1" == "stable" || "$1" == "canary" || "$1" == "testing" ]]; then
   else
     MORIO_CONTAINER_TAG="v$MORIO_VERSION-$CHANNEL"
   fi
-  echo "Building moriod.deb package: $MORIO_FULL_VERSION (channel = $CHANNEL)"
+  echo "Building morio.deb package: $MORIO_FULL_VERSION (channel = $CHANNEL)"
 else
   echo "Invalid distribution channel. Please specify one of: stable, canary, testing."
   exit 1
@@ -26,18 +26,12 @@ fi
 cd $MORIO_GIT_ROOT
 sudo rm -rf build-context
 mkdir build-context
-cp -R pkgs/moriod/* build-context
+cp -R pkgs/morio-client/linux/* build-context
 cd -
 
 # Update version in various files
 cd build-context
-sed -i "s/__MORIO_VERSION__/${MORIO_VERSION}/g" ./DEBIAN/postinst
 sed -i "s/__MORIO_VERSION__/${MORIO_VERSION}/g" ./DEBIAN/control
-sed -i "s/__MORIO_CONTAINER_TAG__/${MORIO_CONTAINER_TAG}/g" ./DEBIAN/postinst
-sed -i "s/__MORIO_RELEASE_CHANNEL__/${CHANNEL}/g" ./DEBIAN/postinst
-sed -i "s/__MORIO_VERSION__/${MORIO_VERSION}/g" ./etc/morio/moriod/version
-sed -i "s/__MORIO_CONTAINER_TAG__/${MORIO_CONTAINER_TAG}/g" ./etc/morio/moriod/channel.env
-sed -i "s/__MORIO_RELEASE_CHANNEL__/${CHANNEL}/g" ./etc/morio/moriod/channel.env
 cd -
 
 # Build the package
@@ -47,9 +41,9 @@ cd -
 
 if [ $? -eq 0 ]
 then
-  echo "Successfully built the moriod.deb package"
+  echo "Successfully built the morio-client.deb package"
 else
-  echo "Failed to build the moriod.deb package"
+  echo "Failed to build the morio-client.deb package"
   exit 1
 fi
 
