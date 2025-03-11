03344a06aa76561eca457051c3a890498225d67c
serge-tellene <114212556+serge-tellene@users.noreply.github.com>
2025-03-11 14:09:59 +0100

    Update install.sh
    Review

--- infra/apt.repo/var/www/install.morio.it/node/testing/install.sh
+++ infra/apt.repo/var/www/install.morio.it/node/testing/install.sh
@@ -9,10 +9,10 @@
 #   This install script will setup a Morio node.
 #   It will:
 #     - Make sure systemd is available
-#     - Detect whether it is a APT or RPM bacsed system
+#     - Detect whether it is an APT or RPM based system
 #     - Setup the morio repository:
 #       - For API: apt.repo.morio.it
-#       - for ROM: rpm.repo.morio.it
+#       - for RPM: rpm.repo.morio.it
 #     - Update dependencies
 #     - Install the moriod package
 #
