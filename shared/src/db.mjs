b3cca51c2135e92dcd373faa45bb71894a3f9d52
Joost De Cock <joost@joost.at>
2025-04-11 17:37:10 +0200

    fix: Read CA cert from disk

--- shared/src/db.mjs
+++ shared/src/db.mjs
@@ -25,7 +25,7 @@ export async function createDbClient (utils, log) {
     : restClient(
       `https://${utils.getSettings('cluster.broker_nodes')[0]}:${utils.getPreset('MORIO_DB_PROXY_PORT')}`,
       {
-        agent: new https.Agent({ ca: getKeys().ca })
+        agent: new https.Agent({ ca })
       }
     )
 
