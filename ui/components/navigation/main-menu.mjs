37c7a09a01c5d4d6f290a2f886d30184b53210b2
levente232 <leventebajusz614@gmail.com>
2025-04-14 04:25:15 -0400

    lint error fix 2

--- ui/components/navigation/main-menu.mjs
+++ ui/components/navigation/main-menu.mjs
@@ -103,9 +103,7 @@ const icons = {
   logs: LogsIcon,
   macs: HardwareIcon,
   metrics: StatusIcon,
-  mods: PuzzleIcon,
   modvars: ModulevarIcon,
-  modfiles: CodeIcon,
   morio: MorioIcon,
   networks: WifiIcon,
   nodes: ServersIcon,
@@ -113,8 +111,6 @@ const icons = {
   oss: WindowIcon,
   pkgs: PackageIcon,
   mods: PuzzleIcon,
-  modvars: VariableIcon,
-  hostvars: VariableIcon,
   modfiles: CodeIcon,
   presets: CheckCircleIcon,
   proxy: Traefik,
@@ -202,28 +198,12 @@ export const links = {
       t: 'Hosts',
       r: 'user',
     },
-    hostvars: {
-      t: 'Host Variables',
-      r: 'user',
-    },
     ips: {
       t: 'IP Addresses',
       r: 'user',
     },
     macs: {
       t: 'MAC Addresses',
-      r: 'user',
-    },
-    mods: {
-      t: 'Modules',
-      r: 'user',
-    },
-    modfiles: {
-      t: 'Module Files',
-      r: 'user',
-    },
-    modvars: {
-      t: 'Module Variables',
       r: 'user',
     },
     oss: {
