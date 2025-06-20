88c1daa781d0ca74258602b1b215c129b685cec8
Joost De Cock <joost@joost.at>
2025-06-20 09:48:22 +0200

    [core] fix: Incorrect path in PM2 config file

--- core/pm2.stable.config.js
+++ core/pm2.stable.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'core',
       script: './dist/index.mjs',
-      cwd: '/morio/api',
+      cwd: '/morio/core',
       max_memory_restart: '250M',
       watch: false,
     },
