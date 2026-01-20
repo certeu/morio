d7e6354cd7936acab741a551a1f420a7e30d7252
Joost De Cock <joost@joost.at>
2026-01-20 09:26:55 +0100

    wip: Started work on morio client for MacOS

--- scripts/build-clients.sh
+++ scripts/build-clients.sh
@@ -15,16 +15,19 @@ CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -a -ldflags '-extldflags "-static
 
 # Build for macOS
 echo " 3/5 macOS on amd64..."
-echo "(skipped - not implemented yet)"
+echo "(skipped - not supported)"
 #CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-macos-amd64
 echo " 4/5 macOS on arm64..."
-echo "(skipped - not implemented yet)"
-#CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-macos-arm64
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-macos-arm64
 
 # Build for Windows
 echo " 5/5 Windows on amd64..."
 echo "(skipped - not implemented yet)"
 #CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-windows-amd64
 
-# Copy linux/amd64 client in place, it's the only one we ship for now
+# Copy linux/amd64 client in place (arm is not handled for now)
 cp ../local/builds/morio-linux-amd64 ../pkgs/morio-client/linux/usr/sbin/morio
+
+# Copy linux/amd64 client in place
+cp ../local/builds/morio-macos-arm64 ../pkgs/morio-client/macos/opt/morio/bin/morio
+
---
+++ scripts/build-morio-client-pkg.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+# Versions
+VERSION=$(cat ../VERSION | xargs)
+BEATS_VERSION=9.2.4
+
+# Directories
+PKG_ROOT="../local/builds/macos/src"
+PKG_BUILD="../local/builds/macos/build"
+PKG_SRC="../pkgs/morio-client/macos"
+
+# Clean and create directories
+rm -rf "$PKG_ROOT"
+rm -rf "$PKG_BUILD"
+mkdir -p "$PKG_ROOT"
+mkdir -p "$PKG_BUILD"
+
+# Copy package source files
+cp -R $PKG_SRC/* $PKG_ROOT
+
+# Copy the morio binary (we assume it was pre-built)
+cp $PKG_ROOT/../../morio-macos-arm64 $PKG_ROOT/opt/morio/bin/morio
+
+# Download and extract Elastic beats
+echo "Downloading Filebeat from Elatic.co..."
+curl -L -o "$PKG_BUILD/filebeat.tar.gz" \
+    "https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${BEATS_VERSION}-darwin-x86_64.tar.gz"
+tar -xzf "$PKG_BUILD/filebeat.tar.gz" -C "$PKG_BUILD"
+cp "$PKG_BUILD/filebeat-${BEATS_VERSION}-darwin-x86_64/filebeat" "$PKG_ROOT/opt/morio/bin/"
+
+echo "Downloading Metricbeat from Elatic.co..."
+curl -L -o "$PKG_BUILD/metricbeat.tar.gz" \
+    "https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${BEATS_VERSION}-darwin-x86_64.tar.gz"
+tar -xzf "$PKG_BUILD/metricbeat.tar.gz" -C "$PKG_BUILD"
+cp "$PKG_BUILD/metricbeat-${BEATS_VERSION}-darwin-x86_64/metricbeat" "$PKG_ROOT/opt/morio/bin/"
+
+# Build package
+echo "Building MacOS morio package..."
+pkgbuild --root "$PKG_ROOT" \
+         --identifier com.morio.client \
+         --version "$VERSION" \
+         --install-location / \
+         --scripts "$PKG_BUILD/scripts" \
+         "morio-$VERSION.pkg"
+
+echo "MacOS morio package built: morio-$VERSION.pkg"
