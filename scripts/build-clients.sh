4649ed04f6ac40a63338d77a7e8df580a5447d40
Joost De Cock <joost@joost.at>
2026-01-22 19:08:03 +0100

    wip: First work on building the MSI

--- scripts/build-clients.sh
+++ scripts/build-clients.sh
@@ -8,26 +8,27 @@ cd $MORIO_GIT_ROOT/client
 echo "Building Morio client:"
 
 # Build for linux
-echo " 1/5 Linux on amd64..."
+echo " 1/4 Linux on amd64..."
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-linux-amd64
-echo " 2/5 Linux on arm64..."
+echo " 2/4 Linux on arm64..."
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-linux-arm64
 
 # Build for macOS
-echo " 3/5 macOS on amd64..."
-echo "(skipped - not supported)"
-#CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-macos-amd64
-echo " 4/5 macOS on arm64..."
+echo " 3/4 MacOS on arm64..."
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-macos-arm64
 
 # Build for Windows
-echo " 5/5 Windows on amd64..."
-echo "(skipped - not implemented yet)"
-#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-windows-amd64
+echo " 4/4 Windows on amd64..."
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-windows-amd64.exe
+# Is there a use case to support Windows on ARM?
+#CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o ../local/builds/morio-windows-amd64.exe
 
 # Copy linux/amd64 client in place (arm is not handled for now)
 cp ../local/builds/morio-linux-amd64 ../pkgs/morio-client/linux/usr/sbin/morio
 
-# Copy linux/amd64 client in place
+# Copy macos/arm64 client in place
 cp ../local/builds/morio-macos-arm64 ../pkgs/morio-client/macos/bin/morio
+
+# Copy windows/amd64 client in place
+cp ../local/builds/morio-windows-amd64.exe ../pkgs/morio-client/windows/bin/morio.exe
 
